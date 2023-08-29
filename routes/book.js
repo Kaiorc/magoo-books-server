3ef758bc2aee11fbb68341e79d6b03b2d9981f7e
Kaio Rodrigues <kaio.cunha@aluno.uece.br>
2023-08-29 02:34:40 -0300

    Rotas de GET, POST, PATCH e DELETE, adicionadas

--- routes/book.js
+++ routes/book.js
@@ -7,8 +7,21 @@ const router = Router();
 /* O "/" é o path, ou seja, o caminho que a aplicação irá acessar */
 /* "req" é a request e "res" é a response, que é o que é devolvido quando chamado */
 router.get('/', (req, res) => {
-    res.send("Hello World! no book.js")
+    res.send("Uma requisição do tipo GET foi feita no book.js")
+})
+
+router.post('/', (req, res) => {
+    res.send("Uma requisição do tipo POST foi feita no book.js")
+})
+
+router.patch('/', (req, res) => {
+    res.send("Uma requisição do tipo PATCH foi feita no book.js")
+})
+
+router.delete('/', (req, res) => {
+    res.send("Uma requisição do tipo DELETE foi feita no book.js")
 })
 
 /* Exportando o router (equivalente ao "export default" do React)*/
-module.exports = router;
+// module.exports = router;
+export default router;
