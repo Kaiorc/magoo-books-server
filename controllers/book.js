7bb3e8ba7ab922794477d563cee5cb10cb475c16
Kaio Rodrigues <kaio.cunha@aluno.uece.br>
2023-09-07 19:35:23 -0300

    Verificação de "ID" nos controllers adicionada

--- controllers/book.js
+++ controllers/book.js
@@ -1,8 +1,6 @@
 /* Os controladores são responsáveis pela lógica da aplicação, */
 /* coordenando que resposta deve ser retornada em cada situação. */
-
 import { getAllBooks, getBookById, insertBook, modifyBook, deleteBookById } from '../services/book.js'
-
 /* "req" é a request e "res" é a response, que é o que é devolvido quando chamado */
 export function getBooks(req, res) {
     try {
@@ -13,18 +11,25 @@ export function getBooks(req, res) {
         res.send(error.message)
     }
 }
-
 export function getBook(req, res) {
     try {
         const id = req.params.id
-        const book = getBookById(id)
-        res.send(book)
+        /* Verificando se o ID foi fornecido corretamente */
+        /* Se "id" não está vazio & "Number(id)" confirma que é um número, */
+        /* entra no bloco, se não, gera uma resposta de status 422 (Unprocessable Entity) */
+        /* e devolve uma resposta "ID inválido" */
+        if (id && Number(id)) {
+            const book = getBookById(id)
+            res.send(book)
+        } else {
+            res.status(422)
+            res.send("ID inválido")
+        }
     } catch (error) {
         res.status(500)
         res.send(error.message)
     }
 }
-
 /* Função que recebe os dados do novo livro e os adiciona no banco de dados (arquivo JSON) */
 /* recebemos os dados do novo livro acessando ".body" que vem dentro da requisição ("req") */
 export function postBook(req, res) {
@@ -38,26 +43,42 @@ export function postBook(req, res) {
         res.send(error.message)
     }
 }
-
 /* Função que recebe os dados do livro a ser modificado e modifica um livro existente com */
 /* base em seu ID e nos dados fornecidos no corpo da requisição. */
 export function patchBook(req, res){
     try {
         const id = req.params.id
         const body = req.body
-        modifyBook(body, id)
-        res.send("Livro modificado com sucesso!")
+        /* Verificando se o ID foi fornecido corretamente */
+        /* Se "id" não está vazio & "Number(id)" confirma que é um número, */
+        /* entra no bloco, se não, gera uma resposta de status 422 (Unprocessable Entity) */
+        /* e devolve uma resposta "ID inválido" */
+        if (id && Number(id)) {
+            modifyBook(body, id)
+            res.send("Livro modificado com sucesso!")
+        } else {
+            res.status(422)
+            res.send("ID inválido")
+        }
     } catch (error) {
         res.status(500)
         res.send(error.message)
     }
 }
-
 export function deleteBook(req, res){
     try {
         const id = req.params.id
-        deleteBookById(id)
-        res.send("Livro deletado com sucesso!")
+        /* Verificando se o ID foi fornecido corretamente */
+        /* Se "id" não está vazio & "Number(id)" confirma que é um número, */
+        /* entra no bloco, se não, gera uma resposta de status 422 (Unprocessable Entity) */
+        /* e devolve uma resposta "ID inválido" */
+        if (id && Number(id)) {
+            deleteBookById(id)
+            res.send("Livro deletado com sucesso!")
+        } else {
+            res.status(422)
+            res.send("ID inválido")
+        }
     } catch (error) {
         res.status(500)
         res.send(error.message)
