44d3cb2148ee9102ed78be63ff45dcbc340e5587
Kaio Rodrigues <kaio.cunha@aluno.uece.br>
2023-09-07 20:54:03 -0300

    Verificação de "ID" e "nome" nos controllers adicionada

--- controllers/book.js
+++ controllers/book.js
@@ -1,6 +1,8 @@
 /* Os controladores são responsáveis pela lógica da aplicação, */
 /* coordenando que resposta deve ser retornada em cada situação. */
+
 import { getAllBooks, getBookById, insertBook, modifyBook, deleteBookById } from '../services/book.js'
+
 /* "req" é a request e "res" é a response, que é o que é devolvido quando chamado */
 export function getBooks(req, res) {
     try {
@@ -11,6 +13,7 @@ export function getBooks(req, res) {
         res.send(error.message)
     }
 }
+
 export function getBook(req, res) {
     try {
         const id = req.params.id
@@ -30,19 +33,30 @@ export function getBook(req, res) {
         res.send(error.message)
     }
 }
+
 /* Função que recebe os dados do novo livro e os adiciona no banco de dados (arquivo JSON) */
 /* recebemos os dados do novo livro acessando ".body" que vem dentro da requisição ("req") */
 export function postBook(req, res) {
     try {
         const newBook = req.body
-        insertBook(newBook)
-        res.status(201)
-        res.send("Livro adicionado com sucesso!")
+        /* Verificando se o "ID" e o "nome" foram fornecidos */
+        /* Se "id" não está vazio & "nome" também, entra no bloco, */
+        /* se não, gera uma resposta de status 422 (Unprocessable Entity) */
+        /* e devolve uma resposta "Dados inválidos: ID e nome são obrigatórios" */
+        if(req.body.id && req.body.nome){
+            insertBook(newBook)
+            res.status(201)
+            res.send("Livro adicionado com sucesso!")
+        } else {
+            res.status(422)
+            res.send("Dados inválidos: ID e nome são obrigatórios")
+        }
     } catch (error) {
         res.status(500)
         res.send(error.message)
     }
 }
+
 /* Função que recebe os dados do livro a ser modificado e modifica um livro existente com */
 /* base em seu ID e nos dados fornecidos no corpo da requisição. */
 export function patchBook(req, res){
@@ -65,6 +79,7 @@ export function patchBook(req, res){
         res.send(error.message)
     }
 }
+
 export function deleteBook(req, res){
     try {
         const id = req.params.id
